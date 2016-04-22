45c7a5aba273c99b7a2a18d7c567291e51fa9ca8
sameera91 <sameera.bachoti@gmail.com>
2016-04-22 13:41:11 -0400

    category feature specs passing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,13 @@
 class CategoriesController < ApplicationController
+  def index
+    @category = Category.all
+  end
+
+  def edit
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
 
 end
