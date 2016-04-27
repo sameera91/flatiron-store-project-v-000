9811317499f1bc109b1cb2cb90d8369872eb00ea
sameera91 <sameera.bachoti@gmail.com>
2016-04-27 11:46:11 -0400

    All tests passing

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,17 +12,26 @@ class Cart < ActiveRecord::Base
     total
   end
 
-  def add_item(item)
-
+  def add_item(item_id)
+=begin
     line_item = self.line_items.find_by(item_id: item)
      if line_item
        quantity = line_item.quantity+1
       line_item.update(quantity: quantity)
+      line_item
      else
-       self.line_items.build(item_id: item, quantity: 1)
-
+       self.line_items.build(item_id: item, cart_id: self.id, quantity: 1)
+       self.line_items
      end
-    line_item
+=end
+
+    if line_item = self.line_items.find_by(:item_id => item_id)
+        quantity = line_item.quantity+1
+      line_item.update(quantity: quantity)
+        line_item           
+      else          
+      self.line_items.build(:item_id => item_id, :cart_id => self.id)     
+      end         
   end  
 
 end
